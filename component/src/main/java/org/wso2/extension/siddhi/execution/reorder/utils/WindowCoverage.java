5843e1e3cda7f36c6c27034e775fa6e5958cb1b8
Vithursa <vithu30@hotmail.com>
2019-04-06 21:26:41 +0530

    Refactored the code

--- component/src/main/java/org/wso2/extension/siddhi/execution/reorder/utils/WindowCoverage.java
+++ component/src/main/java/org/wso2/extension/siddhi/execution/reorder/utils/WindowCoverage.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.extension.siddhi.execution.reorder.utils;
 
 import org.apache.log4j.Logger;
@@ -30,7 +48,6 @@ public class WindowCoverage {
      * @param criticalValue critical value for algorithm
      * @return windowCoverageThreshold
      */
-
     public double calculateWindowCoverageThreshold(double criticalValue, List<Double> listOfData) {
         double mean = calculateMean(listOfData);
         double variance = calculateVariance(listOfData, mean);
@@ -95,7 +112,6 @@ public class WindowCoverage {
      * @param windowSize      Size of window
      * @return runtimeWindowCoverage
      */
-
     public double calculateRuntimeWindowCoverage(List<Long> eventTimestamps, long windowSize) {
 //        long start = System.currentTimeMillis();
         double runtimeWindowCoverage = -1;
